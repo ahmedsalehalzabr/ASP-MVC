5759eea8b7e0d63024ba9bb3f97edd50b9a2f0a0
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-04-05 03:41:52 -1200

    27ُEdit item

--- ASP MVC/Controllers/ItemsController.cs
+++ ASP MVC/Controllers/ItemsController.cs
@@ -19,5 +19,23 @@ namespace ASP_MVC.Controllers
             IEnumerable<Item> itemsList = _db.Items.ToList();
             return View(itemsList);
         }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(Item item)
+        {
+           if(ModelState.IsValid)
+            {
+                _db.Items.Add(item);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           else { return View(item); }
+        }
     }
 }
