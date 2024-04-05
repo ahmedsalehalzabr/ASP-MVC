8411efa5fe38b53a12c9439074ea7b5aed08ebd7
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-04-05 00:17:31 -1200

    Add project files.

---
+++ ASP MVC/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using ASP_MVC.Data;
+using ASP_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_MVC.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        private readonly AppDbContext _db;
+
+        public ItemsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> itemsList = _db.Items.ToList();
+            return View(itemsList);
+        }
+    }
+}
---
+++ ASP MVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASP_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_MVC.Data
+{
+    public class AppDbContext : DbContext  
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+       
+        {
+            
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
